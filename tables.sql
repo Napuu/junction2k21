56df850e8b73d7b507b7db5a435d49821162da02
Napuu <santeri.kaariainen@iki.fi>
2021-11-19 23:24:14 +0200

    add docker-compose + with init script

--- tables.sql
+++ tables.sql
@@ -1,12 +1,12 @@
-CREATE TABLE journeys IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS journeys  (
     id SERIAL PRIMARY KEY,
     j_date DATE,
     start_location INT,
     end_location INT,
     quantity INT
-)
+);
 
-CREATE TABLE route IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS route (
     id INT PRIMARY KEY, -- alku- + loppupostinumero
-    geom LINESTRING
-)
+    geom Geometry(LINESTRING)
+);
